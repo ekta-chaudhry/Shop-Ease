a4700dd539c244cbc1fb772df2034d6348ed151e
Ekta Chaudhry <ekta.chaudhry0701@gmail.com>
2024-07-16 23:48:17 +0530

    Adding Express to the project

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const http = require('http');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/favicon.ico', (req, res)=> res.sendStatus(204));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404Error.html'));
+})
+app.listen(3000);
