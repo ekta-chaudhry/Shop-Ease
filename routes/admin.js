b89395ae46785eb4422c1cc48cad57567a0e41b0
Ekta Chaudhry <ekta.chaudhry0701@gmail.com>
2024-07-18 20:36:00 +0530

    Adding more components

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 const router = express.Router();
 
-router.get('/add-product', productsController.getAddProduct);
-
-router.post('/add-product', productsController.postAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+router.get('/products', adminController.getProducts);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+router.get('/products', shopController.getProducts);
+router.get('/cart', shopController.getCart);
+router.get('/orders', shopController.getOrders);
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
