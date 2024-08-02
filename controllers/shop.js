d66b73846baea5f2ed6a1f71a6cb8d8da9472c53
Ekta Chaudhry <ekta.chaudhry0701@gmail.com>
2024-08-02 21:08:34 +0530

    Adding pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,10 +6,30 @@ const Order = require('../models/order');
 
 const pdfDocument = require('pdfkit');
 
+const ITEMS_PER_PAGE = 1;
+
 exports.getProducts = (req, res, next) => {
-    Product.find()
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().countDocuments()
+    .then(numProducts => {
+        totalItems = numProducts;
+        return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
-        res.render('shop/product-list', {prods: products, pageTitle: 'ALL Products', path: '/products'});
+        res.render('shop/product-list', {
+            prods: products, 
+            pageTitle: 'All Products', 
+            path: '/products', 
+            currentPage: page, 
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems, 
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+        });
     })
     .catch(err => {
         const error = new Error(err);
@@ -19,9 +39,27 @@ exports.getProducts = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-    Product.find()
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().countDocuments()
+    .then(numProducts => {
+        totalItems = numProducts;
+        return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
-        res.render('shop/index', {prods: products, pageTitle: 'My Shop', path: '/'});
+        res.render('shop/index', {
+            prods: products, 
+            pageTitle: 'My Shop', 
+            path: '/', 
+            currentPage: page, 
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems, 
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+        });
     })
     .catch(err => {
         const error = new Error(err);
